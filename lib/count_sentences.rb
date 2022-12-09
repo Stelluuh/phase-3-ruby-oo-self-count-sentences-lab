68f325bc738e78e3a034d893291d1a7fc0aac943
stella <sftamesis13@gmail.com>
2022-12-09 14:12:40 -0800

    commit tree dc7a2e7b82f5b3c5f2f38a7ecc7ac9a2d1824ae5

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    # self.split(/(\?|\.|\!+\s)/).count
+    self.split(/\.|\?|\!/).filter { |sentence| !sentence.empty? }.count
+      #the .empty? checks if the string length is zero or not. 
 
   end
 end
+
+binding.pry
